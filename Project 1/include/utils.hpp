6f0be8a7e0266b49a6f12a7dfdb46e0618fceaab
Vilde Ung <67263002+ungvilde@users.noreply.github.com>
2022-09-07 22:28:28 +0200

    add header file to organize code

---
+++ Project 1/include/utils.hpp
@@ -0,0 +1,23 @@
+// First we add an "include guard". It ensures that this header file can 
+// only end up being included *once* for the compilation of any given .cpp file
+#ifndef __utils_hpp__  
+#define __utils_hpp__
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+
+// Return a string with a double in scientific notation
+std::string scientific_format(double d, const int width=20, const int prec=10);
+
+// write file with uvalues anf xvalues computed at 100 points 
+void write_file(std::string filename, std::vector< std::vector<double> > values);
+
+// exact solution
+double u(double x);
+double f(double x);
+
+#endif  // end of include guard __utils_hpp__
+
---
+++ Project 1/plot.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def readfile(filename):
+    """
+    Read data file and and return a table of x and u values.
+    Input:
+    - filename: name of the file to read
+    """
+    x, u = [], []
+    with open(filename, 'r') as f:
+        for line in f:
+            x.append(float(line.split()[0]))
+            u.append(float(line.split()[1]))
+
+    return np.array(x), np.array(u)
+
+
+def plot(x, u, save=False):
+    """Plot an array of x and u values."""
+    plt.plot(x, u)
+    plt.xlabel(r'$x$')
+    plt.ylabel(r'$u(x)$')
+    if save:
+        plt.savefig('problem_1.pdf')
+    plt.show()
+
+
+if __name__ == '__main__':
+    x, u = readfile("data.txt")
+    plot(x, u)
