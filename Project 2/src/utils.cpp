e518cc2a41f3442a98ad76e91545294dd1487f5b
Vilde Ung <67263002+ungvilde@users.noreply.github.com>
2022-09-24 13:39:39 +0200

    remove unused functions + small plot update

--- Project 2/src/utils.cpp
+++ Project 2/src/utils.cpp
@@ -11,49 +11,6 @@ arma::mat make_tridiag(int N, double a, double d)
   return A;
 }
 
-// Return a string with a double in scientific notation
-std::string scientific_format(const double d, const int width, const int prec)
-{
-  std::stringstream ss;
-  ss << std::setw(width) << std::setprecision(prec) << std::scientific << d;
-  return ss.str();
-}
-
-// Return a string with an armadillo vector in scientific notation
-std::string scientific_format(const arma::vec& v, const int width, const int prec)
-{
-  std::stringstream ss;
-  for(double elem : v)
-  {
-    ss << scientific_format(elem, width, prec);
-  }
-  return ss.str();
-}
-
-void write_file(std::string filename, std::vector< std::vector<double> > values)
-{
-    // this is a more general and usable version
-    // first we create a file and write the data points to this file
-    std::ofstream ofile;
-    ofile.open(filename);
-    
-    // width and precision parameters used to format the output
-    int width = 25;
-    int precision = 15;
-    // Loop over steps
-    int M = values[0].size();
-    for(int j = 0; j < M; j++)
-    {
-    ofile << scientific_format(values[0][j], width, precision) //xvalues
-        << scientific_format(values[1][j], width, precision) //yvalues
-        << std::endl;
-    }
-
-    // close the data file
-    ofile.close();
-
-}
-
 //find analytic eigenvalue/vector solution to tridiagonal matrix A = tridiag(a,d,a) where A in NxN
 void solve_analytic(int N, double a, double d, arma::vec& eigvals, arma::mat& eigvecs)
 { 
