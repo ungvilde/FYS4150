01c3ad6eed0c0ed81f3ebecff0f9f90caa788d97
Even Tobias Eriksen <73366525+eventob@users.noreply.github.com>
2022-12-07 12:56:21 +0100

    Create plot.py

---
+++ Project 5/src/plot.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# PyArmadillo is a Python module that provides a Python interface to the Armadillo C++ linear algebra library.
+import pyarma as pya
+
+# Example of declaring a pyarma matrix
+A = pya.mat()
+A.load(file)
